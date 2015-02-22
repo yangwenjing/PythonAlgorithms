d52eadefbb87fa2f0d8a3d507d8435a79bfcc573
yangwenjing <yangwenjing246@gmail.com>
2015-02-22 21:53:44 +0800

    中级的 去掉排序链表中的重复项

--- PythonAlgorithms/RomoveDupelicates.py
+++ PythonAlgorithms/RomoveDupelicates.py
@@ -15,7 +15,42 @@ class Solution:
 
     def __init__(self):
         self.head = ListNode(1)
-        self.head.next = ListNode(1)
+        self.head.next = ListNode(2)
+        self.head.next.next = ListNode(2)
+
+    def deleteDuplicates2(self, head):
+
+        if head is None:
+            return None
+        node = head
+        node_next = node.next
+        head2 = next_node = None
+
+        while node is not None:
+
+            if node_next is not None and node.val == node_next.val:
+                val = node.val
+                while node_next is not None and node_next.val == val:
+                    node_next = node_next.next
+
+                node = node_next
+                node_next = node_next.next if node_next else None
+
+            else:
+
+                if head2 is None:
+                    head2 = node
+                    head2.next = None
+                    next_node = head2
+                else:
+                    next_node.next = node
+                    next_node = node
+                    next_node.next = None
+
+                node = node_next
+                node_next = node_next.next if node_next else None
+
+        return head2
 
     def deleteDuplicates(self, head):
 
@@ -35,7 +70,7 @@ class Solution:
         return head
 
 s = Solution()
-node = s.deleteDuplicates(s.head)
+node = s.deleteDuplicates2(s.head)
 
 while node is not None:
     print node.val
